f064d6c10637049ee4442822bf3b14fe5e0bea25
Gabriel Com <com.gabriel@uqam.ca>
2015-03-25 08:43:16 -0400

    Début de l'aiguilleur.

--- server/app.js
+++ server/app.js
@@ -1,10 +1,18 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    app = express();
+
+var moodle = require('./moodle/');
 
 var port = process.env.PORT || 3000,
     ip = process.env.IP || '127.0.0.1';
-    
+
 app.use(express.static(process.env.REPERTOIRE_PUBLIC));
+
+
+//Configurer les intergiciels
+app.use('/api/moodle/grades', function(req,res, next) {
+    moodle.getNote();
+});
 
 var server = app.listen(port, ip, function () {
     console.log('YOUPPY!');
