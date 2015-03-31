76f52c3363e6b18fc423defffe8a603587763d99
Gabriel Com <com.gabriel@uqam.ca>
2015-03-31 16:16:48 -0400

    Ajout de quelques commentaires.

--- server/meteo/index.js
+++ server/meteo/index.js
@@ -1,12 +1,26 @@
 var openWeather = require("./lib/openWeather");
 
+/**
+ * Façade vers le microservice de météo.
+ * @module server/meteo
+ * @type {{passerelle: {temperatureActuelle: Function}}}
+ */
 module.exports = {
     passerelle: {
+        /**
+         * Fonction qui fait le lien entre l'api de météo et express
+         * @param req - La requête express
+         * @param res - La réponse express
+         * @param next - La fonction vers le prochain middleware
+         */
         temperatureActuelle: function (req, res, next) {
             var ville = req.params.ville;
             openWeather.temperatureActeulle(ville, function (err, data) {
                 if (!err) {
-                    res.json(data);
+                    res.json({
+                        status : 'success',
+                        data: data
+                    });
                     next();
                 } else {
                     next(err);
--- server/meteo/lib/openWeather.js
+++ server/meteo/lib/openWeather.js
@@ -4,13 +4,16 @@ var request = require('request');
 
 var queryUrl = 'http://api.openweathermap.org/data/2.5/weather?q=%s';   //%s = ville
 
+/**
+ * Petite fonction qui va lire vers l'api REST d'OpenWeather.
+ * @module server/meteo/lib
+ * @param ville - le nom de la ville recherchée pour afficher la température
+ * @param fn - La fonction de retour
+ */
 exports.temperatureActeulle = function (ville, fn) {
     request(util.format(queryUrl, ville), function (error, response, body) {
         if (!error && response.statusCode == 200) {
-           fn(null, {
-               status : 'success',
-               data: JSON.parse(body)
-           });
+           fn(null, JSON.parse(body));
         } else {
             fn(error, body);
         }
